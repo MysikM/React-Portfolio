e7826691000a3b371ec41f4d906db8bb2b3c26bc
Mysik <Mishamysik@gmail.com>
2022-04-12 20:19:27 +0300

    add nav main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import PowerButton from "../subComponents/PowerButton";
 import LogoComponent from "../subComponents/LogoComponent";
 import SocialIcons from "../subComponents/SocialIcons";
+import {NavLink} from "react-router-dom";
 
 const MainContainer = styled.div`
   background: ${props => props.theme.body};
@@ -19,7 +20,58 @@ const MainContainer = styled.div`
 
 const Container = styled.div`
   padding: 2rem;
+`;
+
+const Contact = styled(NavLink)`
+  color: ${props => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BLOG = styled(NavLink)`
+  color: ${props => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const WORK = styled(NavLink)`
+  color: ${props => props.theme.text};
+  position: absolute;
+  top: 50%;
+  left: 2rem;
+  transform:  translate(-50%, -50%) rotate(-90deg);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
   
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const ABOUT = styled(NavLink)`
+  color: ${props => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const SKILLS = styled(NavLink)`
+  color: ${props => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
 `;
 
 function Main(props) {
@@ -29,6 +81,34 @@ function Main(props) {
                 <PowerButton />
                 <LogoComponent />
                 <SocialIcons />
+
+                <Contact target="_blank" to={{pathname:"https://t.me/mysik_3301"}}>
+                    <h2>
+                        Say hi ...
+                    </h2>
+                </Contact>
+                <BLOG to="/blog">
+                    <h2>
+                       Blog
+                    </h2>
+                </BLOG>
+                <WORK to="/work">
+                    <h2>
+                        Work
+                    </h2>
+                </WORK>
+                <BottomBar>
+                    <ABOUT to="/about">
+                        <h2>
+                            About.
+                        </h2>
+                    </ABOUT>
+                    <SKILLS to="/skills">
+                        <h2>
+                            My Skills.
+                        </h2>
+                    </SKILLS>
+                </BottomBar>
             </Container>
         </MainContainer>
     );
