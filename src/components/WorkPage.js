0413c76810c655ee07cdc49826f421be728b8d61
Mysik <Mishamysik@gmail.com>
2022-04-13 12:35:35 +0300

    add animation on work page

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -8,6 +8,7 @@ import { Work } from '../data/WorkData';
 import Card from "../subComponents/Card";
 import {YinYang} from "./AllSvgs";
 import BigTitle from "../subComponents/BigTitle";
+import {motion} from 'framer-motion';
 
 const Box = styled.div`
   background-color: ${props => props.theme.body};
@@ -17,7 +18,7 @@ const Box = styled.div`
   align-items: center;
 `;
 
-const Main = styled.ul`
+const Main = styled(motion.ul)`
   position: fixed;
   top: 12rem;
   left: calc(10rem + 15vw);
@@ -36,6 +37,17 @@ const Rotate = styled.span`
   height: 80px;
   z-index: 1;
 `;
+
+const container = {
+    hidden: {opacity: 0},
+    show: {
+        opacity: 1,
+        transition:{
+            staggerChildren: 0.5,
+            duration: 0.5,
+        }
+    }
+}
 
 function WorkPage(props) {
 
@@ -61,7 +73,7 @@ function WorkPage(props) {
                 <LogoComponent theme='dark' />
                 <SocialIcons theme='dark' />
                 <PowerButton />
-                <Main ref={ref}>
+                <Main ref={ref} variants={container} initial='hidden' animate='show' >
                     {
                         Work.map(d =>
                             <Card
