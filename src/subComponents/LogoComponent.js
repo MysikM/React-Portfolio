af52c937187bb89cf443247302c0b45ba6a1722b
Mysik <Mishamysik@gmail.com>
2022-04-13 09:34:01 +0300

    add skills page

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from "styled-components";
+import {darkTheme} from "../components/Themes";
 
 const Logo = styled.h1`
   display: inline-block;
-  color: ${props => props.theme === 'dark' ? props.theme.text : props.theme.body};
+  color: ${props => props.color === 'dark' ? darkTheme.text : darkTheme.body};
   font-family: 'Pacifico', cursive;
   
   position: fixed;
