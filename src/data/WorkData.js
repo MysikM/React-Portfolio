c08957591555a6c2a021ae86c848f8f29bf00923
Mysik <Mishamysik@gmail.com>
2022-04-13 13:24:34 +0300

    change work data

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,61 +1,61 @@
 export const Work = [
     {
         id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
+        name:"Elecar Landing Page",
+        description:"Responsive site",
+        tags:["js","css","html"],
         
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        demo:"https://mysikm.github.io/-Elecar/",
+        github:"https://github.com/MysikM/-Elecar"
     },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
-        
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
+        name:"Coffee shop",
+        description:"FRESH COFFEE IN THE MORNING",
+        tags:["js","css","html"],
+
+        demo:"https://mysikm.github.io/coffee-shop/",
+        github:"https://github.com/MysikM/coffee-shop"
         
     },
     {
         id:3,
-        name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
-        
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
+        name:"Tetris game",
+        description:"game build on clean js.",
+        tags:["vanilaJS","css","html"],
+
+        demo:"https://mysikm.github.io/Basic-Tetris/",
+        github:"https://github.com/MysikM/Basic-Tetris"
+
     },
     {
         id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
-        
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
-       
-    },{
+        name:"deliciousss",
+        description:"deliciousss site with using ReactJS. With realtime fetching of articles from dev.to using API.",
+        tags:["react","api","sass"],
+
+        demo:"https://mysikm.github.io/deliciousss/",
+        github:"https://github.com/MysikM/deliciousss"
+    },
+    {
         id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using js and css.",
+        tags:["js","css","html"],
+
+        demo:"https://mysikm.github.io/miami-slider/",
+        github:"https://github.com/MysikM/miami-slider"
+       
     },
     {
         id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
+        name:"Device Landing",
+        description:"Site build using html and css",
+        tags:["js","css","html"],
+
+        demo:"https://mysikm.github.io/Device/",
+        github:"https://github.com/MysikM/Device"
         
     },
-    
   
 ] 
